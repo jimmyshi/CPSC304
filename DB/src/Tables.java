13504140b9b368f5c8e1fe462db2ce0e5ee04a53
Jimmy <Jimmy@Jimmy-PC>
2013-11-11 14:44:16 -0800

    Up to date

---
+++ DB/src/Tables.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import javax.swing.table.*;
+
+import java.sql.*;
+import java.util.*;
+
+public class Tables extends AbstractTableModel{
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
+
